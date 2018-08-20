19a18d503e9d59eb2e0cff80e4fcf2c177606a87
Arsh-Sandhu <arsh.binny@gmail.com>
2018-08-20 11:02:26 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ var configValues = require('../config');
 module.exports = {
     getDbConnString: function () {
         // return mongoDB connection string
-        return "mongodb://" + configValues.uname + ':' + configValues.password + '@ds123532.mlab.com:23532/nodetododb';
+        return "mongodb://" + configValues.uname + ':' + configValues.password + '@****.com:***312/nodetododb';
     }
 }
